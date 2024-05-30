0e801e4ae80d099f0fbf02645423eea72641afe6
Richard Körner <grasbock@gmail.com>
2024-05-30 17:14:48 +0700

    graph

--- src/lib/IssueGraph.svelte
+++ src/lib/IssueGraph.svelte
@@ -1,8 +1,73 @@
 <script lang="ts">
     import { Graph } from "./graph";
+    import * as vis from 'vis-network'
+    import {DataSet} from 'vis-data'
+    import { onMount } from "svelte";
 
     export let graph: Graph
+    var network: vis.Network | null = null;
+
+    function draw() {
+        // create an array with nodes
+        var nodes = new DataSet(graph.nodes.map((n, ni) => {
+            return { id: ni, label: `#${n.issue.number}` }; 
+        }));
+
+        // create an array with edges
+        let vis_edges: vis.DataInterfaceEdges
+        let edges: { id: number, from: number, to: number }[] = []
+        let i = 0
+        graph.nodes.forEach((n, ni) => {
+            n.dependencies.forEach((_, di) => {
+                edges.push({ id: i, from: di, to: ni })
+                i++
+            })
+        })
+        vis_edges = new DataSet(edges);
+
+        var data: vis.Data = {
+            nodes: nodes,
+            edges: edges,
+        };
+
+        // create a network
+        var container = document.getElementById("mynetwork");
+        if(container == null){
+            return
+        }
+        var directionInput = "UD";
+        var options: vis.Options = {
+            interaction: {
+                dragNodes: false
+            },
+            edges: {
+                smooth: {
+                    enabled: true,
+                    type: "cubicBezier",
+                    roundness: 0.4,
+                },
+            },
+            nodes: {
+                shape: "box",
+            },
+            layout: {
+                hierarchical: {
+                    direction: directionInput,
+                },
+            },
+        };
+        network = new vis.Network(container, data, options);
+    }
+    onMount(async () => {
+      draw();
+    });
 </script>
 
-<p>Graph</p>
-...
+<div id="mynetwork"></div>
+
+<style>
+    #mynetwork {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
