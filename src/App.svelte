7662d53705be65f9f952b8d18ff9a6895e4b3c38
Richard Körner <grasbock@gmail.com>
2024-06-16 19:33:05 +0700

    implement oauth

--- src/App.svelte
+++ src/App.svelte
@@ -4,28 +4,67 @@
 	import Spinner from './lib/Spinner.svelte';
     import DepViz from './lib/DepViz.svelte';
 
-	let access_token: string|null = "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"
+	function get_code_param(){
+		const queryString = window.location.search;
+		const urlparam = new URLSearchParams(queryString)
+		const codeParam = urlparam.get("code")
+		return codeParam
+	}
+
+	let authorization_code: string|null = get_code_param() // = "blabla"
+	let access_token: string|undefined// = "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"
+	let host = "github"
 	let owner = "octocat"
 	let repo = "Hello-World"
 	let issue_number = 3094
 
-	const octokit = new Octokit({ auth: access_token });   
+	let octokit : Octokit
 
-	// authenticates as app based on request URLs
-	let loading: Promise<any> = octokit.rest.users.getAuthenticated().then(({ data: { login } }) => {
-		console.log("loaded")
-	});
+	let loading: Promise<any> = Promise.resolve()
+	let loading_text = ""
+		
+	async function fetch_access_token(){
+		loading_text = "fetching access token"
+		await new Promise((resolve) => setTimeout(resolve, 3000));
+        access_token = "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";
+	}
+
+	async function authenticate_client(){
+		// authenticates as app based on request URLs
+		loading_text = "authenticating the client"
+		let octokit = new Octokit({ auth: access_token });   
+		const { data: { login } } = await octokit.rest.users.getAuthenticated();
+        console.log("loaded");
+	}
+
+	let client_id = "Ov23li6GSVgimy2efmMK"
+
+	function loginWithGithub(){
+		window.location.assign(`http://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${window.location.href}`)
+	}
+
+	if(access_token){
+		loading = authenticate_client()
+	} 
+	else if(authorization_code){
+		loading = fetch_access_token().then(()=>{
+			loading = authenticate_client()
+		})
+	} 
+	
 </script>
 
 <main>
 	{#await loading}
-		Authenticating<br>
+		{loading_text}<br>
 		<Spinner/>
 	{:then number}
 		{#if access_token}
-			<DepViz bind:access_token={access_token} owner={owner} repo={repo} issue_number={issue_number} />
+			<DepViz authorized_client={octokit} bind:access_token={access_token} owner={owner} repo={repo} issue_number={issue_number} />
 		{:else}
-			Access token missing
+			<button on:click={loginWithGithub}>
+				Login with Github
+			</button>
 		{/if}
 	{:catch error}
 		❌ {error}
